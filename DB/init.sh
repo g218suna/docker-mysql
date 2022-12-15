718f56bf8cfadd8f24ea0fa10ee0d465cf491df0
g218suna <g520218m@mails.cc.ehime-u.ac.jp>
2022-12-16 03:02:15 +0900

    CRLF to LF

--- DB/init.sh
+++ DB/init.sh
@@ -1,7 +1,5 @@
 #! /bin/sh
-
 mysql -u root -proot hand_motion_database < 'docker-entrypoint-initdb.d/sql/create-table.sql'
-
 mysql -u root -proot hand_motion_database < 'docker-entrypoint-initdb.d/sql/thumb-table.sql'
 mysql -u root -proot hand_motion_database < 'docker-entrypoint-initdb.d/sql/index-table.sql'
 mysql -u root -proot hand_motion_database < 'docker-entrypoint-initdb.d/sql/middle-table.sql'
